33b64acccb948cb595755f54b3521c0c1354cce7
Mihkel Riik <riikmihkel@gmail.com>
2022-08-10 22:26:32 +0300

    Initial commit for electron and app UI project.

---
+++ App/src/main.ts
@@ -0,0 +1,76 @@
+import electron, { app, BrowserWindow, globalShortcut } from "electron"
+import path from "path"
+import * as pjson from "../package.json"
+
+let mainWindow: BrowserWindow
+function createWindow() {
+    // Create the browser window.
+
+    mainWindow = new BrowserWindow({
+        width: 1280,
+        height: 720,
+        autoHideMenuBar: false,
+        useContentSize: true,
+        resizable: true,
+        webPreferences: {
+            webSecurity: false,
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+    })
+    let uiPath
+    if (process.env.NODE_ENV !== "development") {
+        uiPath = path.join(path.dirname(process.execPath), "\\public")
+    } else {
+        // TODO: local paths
+        uiPath = path.join(process.cwd(), ".\\ui\\index.html")
+    }
+    mainWindow.loadFile(uiPath)
+    mainWindow.webContents.openDevTools()
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on("ready", () => {
+    createWindow()
+
+    app.on("activate", function () {
+        // On macOS it's common to re-create a window in the app when the
+        // dock icon is clicked and there are no other windows open.
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+    let mousePos = electron.screen.getCursorScreenPoint()
+    console.log(mousePos)
+
+    const ret = globalShortcut.register("CommandOrControl+X", () => {
+        console.log("CommandOrControl+X is pressed")
+    })
+
+    if (!ret) {
+        console.log("registration failed")
+    }
+
+    // Check whether a shortcut is registered.
+    console.log(globalShortcut.isRegistered("CommandOrControl+X"))
+})
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit()
+    }
+})
+
+app.on("will-quit", () => {
+    // Unregister a shortcut.
+    globalShortcut.unregister("CommandOrControl+X")
+
+    // Unregister all shortcuts.
+    globalShortcut.unregisterAll()
+})
+
+// In this file you can include the rest of your app"s specific main process
+// code. You can also put them in separate files and require them here.
